49bb86d748848049261ef64658676f5c6a7ef242
Salvador E. Tropea <salvador@inti.gob.ar>
2020-03-10 14:49:18 -0300

    Moved the module to the root and added a script named kiplot to call it from /usr/bin. Simplified the setup.py to make it work with Debian. Added Debian package files. Added a MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,71 +1,45 @@
-# -*- coding: utf-8 -*-
-
+#!/usr/bin/python3
 import io
 import os
-
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 # Package meta-data.
 NAME = 'kiplot'
 DESCRIPTION = 'Plotting driver for KiCad'
-URL = 'https://github.com/johnbeard/kiplot'
+URL = 'https://github.com/INTI-CMNB/kiplot/'
 EMAIL = 'john.j.beard@gmail.com'
 AUTHOR = 'John Beard'
-REQUIRES_PYTHON = '>=2.7.0'
-
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    'pyyaml',
-    # 'pcbnew'
-]
 
 here = os.path.abspath(os.path.dirname(__file__))
-
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
-
-
+     long_description = '\n' + f.read()
 about = {}
-with open(os.path.join(here, 'src', NAME, '__version__.py')) as f:
-    exec(f.read(), about)
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+     exec(f.read(), about)
 
-
-# Where the magic happens:
-setup(
-    name=NAME,
+setup(name=NAME,
     version=about['__version__'],
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    entry_points={
-         'console_scripts': ['kiplot=kiplot.__main__:main'],
-    },
-    install_requires=REQUIRED,
-    include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    packages=[NAME],
+    package_dir={NAME: NAME},
+    scripts=['src/kiplot'],
+    install_requires=['pyyaml'],
+    classifiers = ['Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
     ],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    platforms   = 'POSIX',
+    license     = 'GPL-3.0'
 )
