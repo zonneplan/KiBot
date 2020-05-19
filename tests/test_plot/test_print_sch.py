e81981db5623f4f88def46e06e3cb87f342cea29
Salvador E. Tropea <salvador@inti.gob.ar>
2020-05-19 11:40:55 -0300

    Added a test for print_sch

---
+++ tests/test_plot/test_print_sch.py
@@ -0,0 +1,32 @@
+"""
+Tests of Printing Schematic files
+
+We test:
+- PDF for bom.sch
+
+For debug information use:
+pytest-3 --log-cli-level debug
+
+"""
+
+import os
+import sys
+# Look for the 'utils' module from where the script is running
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(script_dir))
+# Utils import
+from utils import context
+
+PDF_DIR = ''
+PDF_FILE = 'Schematic.pdf'
+
+
+def test_print_sch():
+    prj = 'bom'
+    ctx = context.TestContext('PrSCH', prj, 'print_sch', PDF_DIR)
+    ctx.run()
+    # Check all outputs are there
+    ctx.expect_out_file(PDF_FILE)
+    # We don't have a project, and we don't want one
+    os.remove(os.path.join(ctx.get_board_dir(), prj+'.pro'))
+    ctx.clean_up()
