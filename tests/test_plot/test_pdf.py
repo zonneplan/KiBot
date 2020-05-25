167263774271ebd1a1fdc7fa4f3a191cc5275d67
Salvador E. Tropea <salvador@inti.gob.ar>
2020-05-25 18:55:11 -0300

    Added simple test for plotting a PCB to PDF,

---
+++ tests/test_plot/test_pdf.py
@@ -0,0 +1,31 @@
+"""
+Tests of PDF format (PCB Plot).
+
+For debug information use:
+pytest-3 --log-cli-level debug
+"""
+
+import os
+import sys
+# Look for the 'utils' module from where the script is running
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(script_dir))
+# Utils import
+from utils import context
+
+
+PS_DIR = 'PDF'
+
+
+def test_pdf():
+    prj = 'simple_2layer'
+    ctx = context.TestContext('Plot_PDF', prj, 'pdf', PS_DIR)
+    ctx.run()
+
+    f_cu = ctx.get_gerber_filename('F_Cu', '.pdf')
+    f_silk = ctx.get_gerber_filename('B_Silks', '.pdf')
+    ctx.expect_out_file(f_cu)
+    ctx.expect_out_file(f_silk)
+    ctx.dont_expect_out_file(ctx.get_gerber_job_filename())
+
+    ctx.clean_up()
