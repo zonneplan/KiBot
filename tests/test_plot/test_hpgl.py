8f929db8967f959c7d0fbe07ed169007e9c35125
Salvador E. Tropea <salvador@inti.gob.ar>
2020-05-25 18:42:56 -0300

    Added simple HPGL test

---
+++ tests/test_plot/test_hpgl.py
@@ -0,0 +1,31 @@
+"""
+Tests of HPGL format.
+
+For debug information use:
+pytest-3 --log-cli-level debug
+"""
+
+import os
+import sys
+# Look for the 'utils' module from where the script is running
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(script_dir))
+# Utils import
+from utils import context
+
+
+PS_DIR = 'HPGL'
+
+
+def test_hpgl():
+    prj = 'simple_2layer'
+    ctx = context.TestContext('HPGL', prj, 'hpgl', PS_DIR)
+    ctx.run()
+
+    f_cu = ctx.get_gerber_filename('F_Cu', '.plt')
+    f_silk = ctx.get_gerber_filename('B_Silks', '.plt')
+    ctx.expect_out_file(f_cu)
+    ctx.expect_out_file(f_silk)
+    ctx.dont_expect_out_file(ctx.get_gerber_job_filename())
+
+    ctx.clean_up()
