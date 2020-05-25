9c6ee4f4579777f33098a5f2ff6a7f279f32102f
Salvador E. Tropea <salvador@inti.gob.ar>
2020-05-25 18:59:00 -0300

    Added simple DXF test

---
+++ tests/test_plot/test_dxf.py
@@ -0,0 +1,31 @@
+"""
+Tests of DXF format.
+
+For debug information use:
+pytest-3 --log-cli-level debug
+"""
+
+import os
+import sys
+# Look for the 'utils' module from where the script is running
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(script_dir))
+# Utils import
+from utils import context
+
+
+PS_DIR = 'DXF'
+
+
+def test_dxf():
+    prj = 'simple_2layer'
+    ctx = context.TestContext('DXF', prj, 'dxf', PS_DIR)
+    ctx.run()
+
+    f_cu = ctx.get_gerber_filename('F_Cu', '.dxf')
+    f_fab = ctx.get_gerber_filename('F_Fab', '.dxf')
+    ctx.expect_out_file(f_cu)
+    ctx.expect_out_file(f_fab)
+    ctx.dont_expect_out_file(ctx.get_gerber_job_filename())
+
+    ctx.clean_up()
