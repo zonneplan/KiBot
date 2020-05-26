31e461ca9ca99a9878a82e81e421452b8697e087
Salvador E. Tropea <salvador@inti.gob.ar>
2020-05-26 13:27:49 -0300

    Added 3 tests for wrong layer names.

--- tests/test_plot/test_yaml_errors.py
+++ tests/test_plot/test_yaml_errors.py
@@ -5,6 +5,10 @@ Tests various errors in the config file
 - Wrong kiplot.version
 - Missing drill map type
 - Wrong drill map type
+- Wrong layer:
+  - Incorrect name
+  - Inner.1, but no inner layers
+  - Inner_1 (malformed)
 
 For debug information use:
 pytest-3 --log-cli-level debug
@@ -18,7 +22,7 @@ sys.path.insert(0, os.path.dirname(prev_dir))
 # Utils import
 from utils import context
 sys.path.insert(0, os.path.dirname(prev_dir))
-from kiplot.misc import (EXIT_BAD_CONFIG)
+from kiplot.misc import (EXIT_BAD_CONFIG, PLOT_ERROR)
 
 
 def test_no_version():
@@ -46,4 +50,25 @@ def test_drill_map_wrong_type():
     ctx = context.TestContext('ErrorDrillMapWrongType', '3Rs', 'error_drill_map_wrong_type', None)
     ctx.run(EXIT_BAD_CONFIG)
     assert ctx.search_err("Unknown drill map type: bogus")
+    ctx.clean_up()
+
+
+def test_wrong_layer_1():
+    ctx = context.TestContext('ErrorWrongLayer1', '3Rs', 'error_wrong_layer_1', None)
+    ctx.run(EXIT_BAD_CONFIG)
+    assert ctx.search_err("Unknown layer name: F.Bogus")
+    ctx.clean_up()
+
+
+def test_wrong_layer_2():
+    ctx = context.TestContext('ErrorWrongLayer2', '3Rs', 'error_wrong_layer_2', None)
+    ctx.run(PLOT_ERROR)
+    assert ctx.search_err("Inner layer 1 is not valid for this board")
+    ctx.clean_up()
+
+
+def test_wrong_layer_3():
+    ctx = context.TestContext('ErrorWrongLayer3', '3Rs', 'error_wrong_layer_3', None)
+    ctx.run(EXIT_BAD_CONFIG)
+    assert ctx.search_err("Malformed inner layer name: Inner_1,")
     ctx.clean_up()
