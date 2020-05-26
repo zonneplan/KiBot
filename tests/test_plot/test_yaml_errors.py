eb77e20f80f98455a1386879f836c632e5238161
Salvador E. Tropea <salvador@inti.gob.ar>
2020-05-26 13:04:56 -0300

    Added test for missing drill map type

--- tests/test_plot/test_yaml_errors.py
+++ tests/test_plot/test_yaml_errors.py
@@ -3,6 +3,7 @@ Tests various errors in the config file
 
 - No kiplot.version
 - Wrong kiplot.version
+- Missing drill map type
 
 For debug information use:
 pytest-3 --log-cli-level debug
@@ -30,4 +31,11 @@ def test_wrong_version():
     ctx = context.TestContext('ErrorWrongVersion', '3Rs', 'error_wrong_version', None)
     ctx.run(EXIT_BAD_CONFIG)
     assert ctx.search_err('Unknown KiPlot config version: 20')
+    ctx.clean_up()
+
+
+def test_drill_map_no_type():
+    ctx = context.TestContext('ErrorDrillMapNoType', '3Rs', 'error_drill_map_no_type', None)
+    ctx.run(EXIT_BAD_CONFIG)
+    assert ctx.search_err("Missing `type' in drill map section")
     ctx.clean_up()
