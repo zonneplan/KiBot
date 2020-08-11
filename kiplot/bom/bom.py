ace151f1efcbe70cea2df1cfeb84456551ff6b3c
SET <salvador@inti.gob.ar>
2020-08-11 13:40:14 -0300

    Removed dead code in Joiner class.

--- kiplot/bom/bom.py
+++ kiplot/bom/bom.py
@@ -119,13 +119,11 @@ class Joiner:
         else:
             self.stack.append(((P, N), (P, N)))
 
-    def flush(self, sep, N=None, dash='-'):
+    def flush(self, sep, dash='-'):
         refstr = u''
         c = 0
         for Q in self.stack:
-            if bool(N) and c != 0 and c % N == 0:
-                refstr += u'\n'
-            elif c != 0:
+            if c != 0:
                 refstr += sep
             S, E = Q
             if S == E:
@@ -133,9 +131,6 @@ class Joiner:
                 c += 1
             else:
                 # Do we have space?
-                if bool(N) and (c + 1) % N == 0:
-                    refstr += u'\n'
-                    c += 1
                 refstr += "%s%d%s%s%d" % (S[0], S[1], dash, E[0], E[1])
                 c += 2
         return refstr
