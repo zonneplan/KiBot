e29c7b2a930d4f52bba402e97fed07d31c172e15
SET <salvador@inti.gob.ar>
2020-08-15 15:25:02 -0300

    Fixed tests adding to sys.path unconditionally

--- tests/test_plot/test_yaml_errors.py
+++ tests/test_plot/test_yaml_errors.py
@@ -51,11 +51,14 @@ pytest-3 --log-cli-level debug
 import os
 import sys
 # Look for the 'utils' module from where the script is running
-prev_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.dirname(prev_dir))
+prev_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if prev_dir not in sys.path:
+    sys.path.insert(0, prev_dir)
 # Utils import
 from utils import context
-sys.path.insert(0, os.path.dirname(prev_dir))
+prev_dir = os.path.dirname(prev_dir)
+if prev_dir not in sys.path:
+    sys.path.insert(0, prev_dir)
 from kiplot.misc import (EXIT_BAD_CONFIG, PLOT_ERROR, BOM_ERROR)
 
 
