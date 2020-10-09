0264d0e88f3be9292e4d36d72d50a7c2cee5fe3f
Salvador E. Tropea <salvador@inti.gob.ar>
2020-10-09 11:28:15 -0300

    Added KiAuto as PyPi dependency.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='kibot',
       # Packages are marked using __init__.py
       packages=find_packages(),
       scripts=['src/kibot', 'src/kiplot'],
-      install_requires=['pyyaml', 'xlsxwriter'],
+      install_requires=['kiauto', 'pyyaml', 'xlsxwriter'],
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Console',
                    'Intended Audience :: Developers',
