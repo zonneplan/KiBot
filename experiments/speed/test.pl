8dd92022f83f3aca165bc9da616d5e23cf418828
Salvador E. Tropea <salvador@inti.gob.ar>
2020-10-16 20:23:10 -0300

    Updated the scripts to meassure the speed.

--- experiments/speed/test.pl
+++ experiments/speed/test.pl
@@ -1,4 +1,4 @@
 for ($i=0; $i<100; $i++)
    {
-    system("src/kiplot -b tests/board_samples/good-project.kicad_pcb -c tests/yaml_samples/simple_position_csv.kiplot.yaml -d pp");
+    system("../../src/kiplot -b ../../tests/board_samples/kicad_5/good-project.kicad_pcb -c ../../tests/yaml_samples/simple_position_csv.kibot.yaml -d pp");
    }
