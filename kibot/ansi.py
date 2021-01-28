c31c2c17c22bbfa21bca3e306102d816045dde73
Salvador E. Tropea <salvador@inti.gob.ar>
2021-01-28 15:37:44 -0300

    Removed trailing spaces (flake8)

--- kibot/ansi.py
+++ kibot/ansi.py
@@ -42,16 +42,16 @@ class AnsiCodes(object):
 # class AnsiCursor(object):
 #     def UP(self, n=1):
 #         return CSI + str(n) + 'A'
-# 
+#
 #     def DOWN(self, n=1):
 #         return CSI + str(n) + 'B'
-# 
+#
 #     def FORWARD(self, n=1):
 #         return CSI + str(n) + 'C'
-# 
+#
 #     def BACK(self, n=1):
 #         return CSI + str(n) + 'D'
-# 
+#
 #     def POS(self, x=1, y=1):
 #         return CSI + str(y) + ';' + str(x) + 'H'
 
