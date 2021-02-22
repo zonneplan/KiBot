9e33b7a774c0e6643bd85eab66f8532ac236221c
Salvador E. Tropea <salvador@inti.gob.ar>
2021-02-22 12:25:14 -0300

    Trying to force KiAuto record on GitLab CI/CD

--- kibot/kiplot.py
+++ kibot/kiplot.py
@@ -155,7 +155,9 @@ def exec_with_retry(cmd):
 def add_extra_options(cmd):
     if GS.debug_enabled:
         cmd.insert(1, '-'+'v'*GS.debug_level)
-        # cmd.insert(1, '-r')
+    if GS.debug_enabled or 'GITLAB_CI' in os.environ:
+        # Forcing record on GitLab CI/CD (black magic)
+        cmd.insert(1, '-r')
     if GS.global_kiauto_time_out_scale:
         cmd.insert(1, str(GS.global_kiauto_time_out_scale))
         cmd.insert(1, '--time_out_scale')
