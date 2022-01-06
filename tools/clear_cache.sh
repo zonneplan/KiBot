a9a7c66eab05b73de6046a2ee2d4a2912b4d45f8
Salvador E. Tropea <salvador@inti.gob.ar>
2022-01-06 14:16:51 -0300

    Updated tools/clear_cache.sh to solve pytest issues
    - After updating macros.py pytest messes all and needs time-stamps
      to be reseted

--- tools/clear_cache.sh
+++ tools/clear_cache.sh
@@ -1,1 +1,2 @@
 rm -r ./kibot/bom/__pycache__ ./kibot/kicad/__pycache__ ./kibot/mcpyrate/__pycache__ ./kibot/__pycache__ ./tests/.config/kiplot/plugins/__pycache__ ./tests/.config/kibot/plugins/__pycache__ ./tests/utils/__pycache__ ./tests/test_plot/fake_pcbnew/__pycache__ ./tests/test_plot/__pycache__ ./tests/__pycache__
+touch kibot/*.py
