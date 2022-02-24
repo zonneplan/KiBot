1a5e562d14f25e21bdd5bc6cb9ec83e987a45e3b
Salvador E. Tropea <salvador@inti.gob.ar>
2022-02-24 12:59:44 -0300

    Updated copyright year

--- kibot/__main__.py
+++ kibot/__main__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020-2021 Salvador E. Tropea
-# Copyright (c) 2020-2021 Instituto Nacional de Tecnología Industrial
+# Copyright (c) 2020-2022 Salvador E. Tropea
+# Copyright (c) 2020-2022 Instituto Nacional de Tecnología Industrial
 # Copyright (c) 2018 John Beard
 # License: GPL-3.0
 # Project: KiBot (formerly KiPlot)
