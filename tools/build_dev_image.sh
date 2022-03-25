4efa08c3c485908f3ab7c83eb4ad4ad750fc94d1
Salvador E. Tropea <salvador@inti.gob.ar>
2022-03-25 10:05:59 -0300

    Added scripts to generate the `dev` images

---
+++ tools/build_dev_image.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build -f tools/dev_image/Dockerfile -t setsoft/kicad_auto:dev .
---
+++ tools/build_dev_image_k6.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build -f tools/dev_image_k6/Dockerfile -t setsoft/kicad_auto:dev_k6 .
