4765121e3fa3d36d319c2266c40f8a5b03901edd
Salvador E. Tropea <salvador@inti.gob.ar>
2022-07-13 06:32:11 -0300

    [Tests] ImageMagick available again

--- tests/test_plot/test_dep_downloader.py
+++ tests/test_plot/test_dep_downloader.py
@@ -121,8 +121,9 @@ def test_dep_gs(test_dir, caplog, monkeypatch):
     os.remove(os.path.join(ctx.output_dir, bin_dir, 'gs'))
 
 
+# @pytest.mark.xfail(True, reason="URL down", run=True, raises=AssertionError)
+# https://imagemagick.org/archive/binaries/magick
 @pytest.mark.slow
-@pytest.mark.xfail(True, reason="URL down", run=True, raises=AssertionError)
 def test_dep_convert(test_dir, caplog, monkeypatch):
     """ Check the convert_downloader """
     # Create a context to get an output directory
