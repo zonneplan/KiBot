99c0b33bd001c35e05acfbf3db1213b94f2f4ca8
Salvador E. Tropea <salvador@inti.gob.ar>
2022-07-13 10:38:56 -0300

    [GitHub Action] Added versions to the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -172,4 +172,23 @@ function main {
     run
 }
 
+echo "*****************************************************************************************"
+echo "*****************************************************************************************"
+echo
+echo
+echo KiBot GitHub Action v2
+echo
+echo
+kibot --version
+dpkg -l kicad | grep kicad
+echo Debian: `cat /etc/debian_version`
+pcbnew_do --version
+kicost --version
+pcbdraw --version
+echo "iBoM:" `INTERACTIVE_HTML_BOM_NO_DISPLAY=True generate_interactive_bom.py --version 2> /dev/null | grep "^v"`
+echo
+echo
+echo "*****************************************************************************************"
+echo "*****************************************************************************************"
+
 main "$@"
