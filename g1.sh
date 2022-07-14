b70d2b0b1da45820dee6ae4d52134267bfce60a6
Salvador E. Tropea <salvador@inti.gob.ar>
2022-07-14 19:52:49 -0300

    [CI/CD] Rebalanced groups and combined coverage (experimental)

--- g1.sh
+++ g1.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 # Fast tests
 pytest -v --durations=0 -m "not slow" -n 2 --test_dir=output
-# KiCad2Step tests
-pytest -v --durations=0 -m "slow and (not (pcbnew or eeschema))" --test_dir=output
--- g2.sh
+++ g2.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Eeschema tests
 pytest -v --durations=0 -m "eeschema" --test_dir=output
+# KiCad2Step tests and others
+pytest -v --durations=0 -m "slow and (not (pcbnew or eeschema))" --test_dir=output
