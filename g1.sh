cc2272e7028e43a2f880d06fc7c6467bfd7869fb
Salvador E. Tropea <salvador@inti.gob.ar>
2023-02-06 11:21:52 -0300

    [Tests] Enabling all tests, git should work now

--- g1.sh
+++ g1.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 # Fast tests
-cat ~/.gitconfig
-cp /root/.gitconfig /etc/gitconfig
-cat /root/.gitconfig
-echo "[safe]" >> /etc/gitconfig
-echo "	directory = *" >> /etc/gitconfig
-ls -la /__w/KiBot/KiBot
-pytest-3 -v --durations=0 -m "not slow" -n 2 --test_dir=output -k test_sch_replace_1
+pytest-3 -v --durations=0 -m "not slow" -n 2 --test_dir=output
--- g2.sh
+++ g2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 # Eeschema tests
-#pytest-3 -v --durations=0 -m "eeschema" --test_dir=output
+pytest-3 -v --durations=0 -m "eeschema" --test_dir=output
 # KiCad2Step tests and others
-#pytest-3 -v --durations=0 -m "slow and (not (pcbnew or eeschema))" --log-cli-level debug --test_dir=output
+pytest-3 -v --durations=0 -m "slow and (not (pcbnew or eeschema))" --log-cli-level debug --test_dir=output
