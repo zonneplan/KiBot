460b1a2dc3acf5f2205300e0135da67a124ce3fa
Salvador E. Tropea <salvador@inti.gob.ar>
2023-02-06 10:56:34 -0300

    [Tests] Trying to figure out git issues

--- g1.sh
+++ g1.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Fast tests
-pytest-3 -v --durations=0 -m "not slow" -n 2 --test_dir=output
+cat ~/.gitconfig
+ls -la /__w/KiBot/KiBot
+pytest-3 -v --durations=0 -m "not slow" -n 2 --test_dir=output -k test_sch_replace_1
