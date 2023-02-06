1e6189f637508c3196875467ca72ec19ffb70583
Salvador E. Tropea <salvador@inti.gob.ar>
2023-02-06 11:01:10 -0300

    [Tests] Copying root options to global

--- g1.sh
+++ g1.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Fast tests
 cat ~/.gitconfig
+cp /root/.gitconfig /etc/gitconfig
+cat /root/.gitconfig
 ls -la /__w/KiBot/KiBot
 pytest-3 -v --durations=0 -m "not slow" -n 2 --test_dir=output -k test_sch_replace_1
