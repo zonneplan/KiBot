92fa889cfb3fc28745e8dec9c25fb55c091c636a
Salvador E. Tropea <salvador@inti.gob.ar>
2023-03-16 13:03:32 -0300

    [CI/CD] Removed the Blender version from non-full tags

--- tools/dev_image_k6/build.sh
+++ tools/dev_image_k6/build.sh
@@ -5,9 +5,8 @@ TG1=`docker run --rm ghcr.io/inti-cmnb/kicad6_auto:dev kibot --version | sed 's/
 HASH=`git log --pretty=format:%h -1 | tr -d '\n'`
 TG2=k`docker run --rm ghcr.io/inti-cmnb/kicad6_auto:dev kicad_version.py`
 TG3=d`docker run --rm ghcr.io/inti-cmnb/kicad6_auto:dev cat /etc/debian_version | tr -d '\n'`
-TG4=b`docker run --rm ghcr.io/inti-cmnb/kicad6_auto:latest /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
-docker tag ghcr.io/inti-cmnb/kicad6_auto:dev ghcr.io/inti-cmnb/kicad6_auto:dev_${TG1}-${HASH}_${TG2}_${TG3}_${TG4}
-docker tag ghcr.io/inti-cmnb/kicad6_auto:dev ghcr.io/inti-cmnb/kicad_auto:dev_k6_${TG1}-${HASH}_${TG2}_${TG3}_${TG4}
-docker push ghcr.io/inti-cmnb/kicad6_auto:dev_${TG1}-${HASH}_${TG2}_${TG3}_${TG4}
-docker push ghcr.io/inti-cmnb/kicad_auto:dev_k6_${TG1}-${HASH}_${TG2}_${TG3}_${TG4}
+docker tag ghcr.io/inti-cmnb/kicad6_auto:dev ghcr.io/inti-cmnb/kicad6_auto:dev_${TG1}-${HASH}_${TG2}_${TG3}
+docker tag ghcr.io/inti-cmnb/kicad6_auto:dev ghcr.io/inti-cmnb/kicad_auto:dev_k6_${TG1}-${HASH}_${TG2}_${TG3}
+docker push ghcr.io/inti-cmnb/kicad6_auto:dev_${TG1}-${HASH}_${TG2}_${TG3}
+docker push ghcr.io/inti-cmnb/kicad_auto:dev_k6_${TG1}-${HASH}_${TG2}_${TG3}
 docker push ghcr.io/inti-cmnb/kicad6_auto:dev
