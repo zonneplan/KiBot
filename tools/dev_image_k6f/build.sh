6a6c06dc3341303385bcd75815c8320750c95251
Salvador E. Tropea <salvador@inti.gob.ar>
2023-08-14 13:12:34 -0300

    [CI/CD] Adjusted dev images creation
    - To Debian 12

--- tools/dev_image_k6f/build.sh
+++ tools/dev_image_k6f/build.sh
@@ -5,7 +5,7 @@ TG1=`docker run --rm ghcr.io/inti-cmnb/kicad6_auto_full:dev kibot --version | se
 HASH=`git log --pretty=format:%h -1 | tr -d '\n'`
 TG2=k`docker run --rm ghcr.io/inti-cmnb/kicad6_auto_full:dev kicad_version.py`
 TG3=d`docker run --rm ghcr.io/inti-cmnb/kicad6_auto_full:dev cat /etc/debian_version | tr -d '\n'`
-TG4=b`docker run --rm ghcr.io/inti-cmnb/kicad6_auto_full:latest /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
+TG4=b`docker run --rm ghcr.io/inti-cmnb/kicad6_auto_full:dev /bin/bash -c "blender --version | head -n 1 | tr -d 'Blender '"`
 docker tag ghcr.io/inti-cmnb/kicad6_auto_full:dev ghcr.io/inti-cmnb/kicad6_auto_full:dev_${TG1}-${HASH}_${TG2}_${TG3}_${TG4}
 docker tag ghcr.io/inti-cmnb/kicad6_auto_full:dev ghcr.io/inti-cmnb/kicad_auto_full:dev_k6_${TG1}-${HASH}_${TG2}_${TG3}_${TG4}
 docker tag ghcr.io/inti-cmnb/kicad6_auto_full:dev ghcr.io/inti-cmnb/kicad_auto_full:dev_k6
