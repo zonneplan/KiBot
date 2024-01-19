e94c4a0ec1bcb871230d42acb8aae636eb334127
Salvador E. Tropea <salvador@inti.gob.ar>
2024-01-19 11:07:00 -0300

    [Tests][Added] Outputs help in separated files

--- tests/test_plot/test_misc.py
+++ tests/test_plot/test_misc.py
@@ -431,11 +431,20 @@ def test_help_outputs_md(test_dir):
     ctx.clean_up()
 
 
-def test_help_outputs_rst(test_dir):
+def test_help_outputs_rst_1(test_dir):
     ctx = context.TestContext(test_dir, '3Rs', 'pre_and_position')
     ctx.run(extra=['--help-outputs', '--rst'], no_verbose=True, no_out_dir=True, no_yaml_file=True, no_board_file=True)
     assert ctx.search_out('Gerber format')
     assert ctx.search_out('Type: .?.?gerber.?.?')
+    ctx.clean_up()
+
+
+def test_help_outputs_rst_2(test_dir):
+    """ Separated files """
+    ctx = context.TestContext(test_dir, '3Rs', 'pre_and_position')
+    ctx.run(extra=['--help-outputs', '--rst'], no_verbose=True, no_yaml_file=True, no_board_file=True)
+    ctx.search_out('outputs/gerber')
+    ctx.expect_out_file('gerber.rst', sub=True)
     ctx.clean_up()
 
 
