697585ec500ec382f58805122a55bbb25aa6bf9b
Salvador E. Tropea <salvador@inti.gob.ar>
2024-03-26 12:35:57 -0300

    [CI/CD][Added][KiCad 8] dev images creation

---
+++ tools/dev_image_k8/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+HASH=`git log --pretty=format:%h -1 | tr -d '\n'`
+docker build -f Dockerfile --build-arg repo_hash=${HASH} -t ghcr.io/inti-cmnb/kicad8_auto:dev .
+TG1=`docker run --rm ghcr.io/inti-cmnb/kicad8_auto:dev kibot --version | sed 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\) .*/\1/' | tr -d '\n'`
+TG2=k`docker run --rm ghcr.io/inti-cmnb/kicad8_auto:dev kicad_version.py`
+TG3=d_sid
+docker tag ghcr.io/inti-cmnb/kicad8_auto:dev ghcr.io/inti-cmnb/kicad8_auto:dev_${TG1}-${HASH}_${TG2}_${TG3}
+docker tag ghcr.io/inti-cmnb/kicad8_auto:dev ghcr.io/inti-cmnb/kicad_auto:dev_k8_${TG1}-${HASH}_${TG2}_${TG3}
+docker tag ghcr.io/inti-cmnb/kicad8_auto:dev ghcr.io/inti-cmnb/kicad_auto:dev_k8
+docker push ghcr.io/inti-cmnb/kicad8_auto:dev_${TG1}-${HASH}_${TG2}_${TG3}
+docker push ghcr.io/inti-cmnb/kicad_auto:dev_k8_${TG1}-${HASH}_${TG2}_${TG3}
+docker push ghcr.io/inti-cmnb/kicad_auto:dev_k8
+docker push ghcr.io/inti-cmnb/kicad8_auto:dev
---
+++ tools/dev_image_k8f/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+export HASH=`git log --pretty=format:%h -1 | tr -d '\n'`
+docker build -f Dockerfile --build-arg repo_hash=${HASH} -t ghcr.io/inti-cmnb/kicad8_auto_full:dev .
+TG1=`docker run --rm ghcr.io/inti-cmnb/kicad8_auto_full:dev kibot --version | sed 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\) .*/\1/' | tr -d '\n'`
+TG2=k`docker run --rm ghcr.io/inti-cmnb/kicad8_auto_full:dev kicad_version.py`
+TG3=d_sid
+TG4=b`docker run --rm ghcr.io/inti-cmnb/kicad8_auto_full:dev /bin/bash -c "blender --version | head -n 1 | tr -d 'Blender '"`
+docker tag ghcr.io/inti-cmnb/kicad8_auto_full:dev ghcr.io/inti-cmnb/kicad8_auto_full:dev_${TG1}-${HASH}_${TG2}_${TG3}_${TG4}
+docker tag ghcr.io/inti-cmnb/kicad8_auto_full:dev ghcr.io/inti-cmnb/kicad_auto_full:dev_k8_${TG1}-${HASH}_${TG2}_${TG3}_${TG4}
+docker tag ghcr.io/inti-cmnb/kicad8_auto_full:dev ghcr.io/inti-cmnb/kicad_auto_full:dev_k8
+docker push ghcr.io/inti-cmnb/kicad8_auto_full:dev_${TG1}-${HASH}_${TG2}_${TG3}_${TG4}
+docker push ghcr.io/inti-cmnb/kicad_auto_full:dev_k8_${TG1}-${HASH}_${TG2}_${TG3}_${TG4}
+docker push ghcr.io/inti-cmnb/kicad_auto_full:dev_k8
+docker push ghcr.io/inti-cmnb/kicad8_auto_full:dev
