cddad43d8e83c155023e143eaa1bf28eddcf202c
Salvador E. Tropea <salvador@inti.gob.ar>
2024-04-03 10:30:54 -0300

    [CI/CD] Allow KiBot versions like 1.6.5.1

--- tools/dev_image_k7f/build.sh
+++ tools/dev_image_k7f/build.sh
@@ -2,7 +2,7 @@
 set -e
 export HASH=`git log --pretty=format:%h -1 | tr -d '\n'`
 docker build -f Dockerfile --build-arg repo_hash=${HASH} -t ghcr.io/inti-cmnb/kicad7_auto_full:dev .
-TG1=`docker run --rm ghcr.io/inti-cmnb/kicad7_auto_full:dev kibot --version | sed 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\) .*/\1/' | tr -d '\n'`
+TG1=`docker run --rm ghcr.io/inti-cmnb/kicad7_auto_full:dev kibot --version | sed 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\(\.[0-9]\+\)\?\) .*/\1/' | tr -d '\n'`
 TG2=k`docker run --rm ghcr.io/inti-cmnb/kicad7_auto_full:dev kicad_version.py`
 TG3=d`docker run --rm ghcr.io/inti-cmnb/kicad7_auto_full:dev cat /etc/debian_version | tr -d '\n'`
 TG4=b`docker run --rm ghcr.io/inti-cmnb/kicad7_auto_full:dev /bin/bash -c "blender --version | head -n 1 | tr -d 'Blender '"`
