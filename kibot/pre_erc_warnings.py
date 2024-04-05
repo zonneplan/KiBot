aa909070da1d804194d85f1e50034ebbbcd4297e
Salvador E. Tropea <salvador@inti.gob.ar>
2024-04-05 13:26:02 -0300

    [ERC_Warnings][Added] Deprecation warning

--- kibot/pre_erc_warnings.py
+++ kibot/pre_erc_warnings.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2021 Salvador E. Tropea
-# Copyright (c) 2021 Instituto Nacional de Tecnología Industrial
-# License: GPL-3.0
+# Copyright (c) 2021-2024 Salvador E. Tropea
+# Copyright (c) 2021-2024 Instituto Nacional de Tecnología Industrial
+# License: AGPL-3.0
 # Project: KiBot (formerly KiPlot)
+from .misc import W_DEPR
 from .macros import macros, pre_class  # noqa: F401
 from .error import (KiPlotConfigurationError)
+from .log import get_logger
+logger = get_logger(__name__)
 
 
 @pre_class
 class ERC_Warnings(BasePreFlight):  # noqa: F821
-    """ [boolean=false] **Deprecated**, use the `warnings_as_errors` option from `run_erc`.
+    """ [boolean=false] **Deprecated**, use the `warnings_as_errors` option from `run_erc`/`erc`.
         Option for `run_erc`. ERC warnings are considered errors """
     def __init__(self, name, value):
         super().__init__(name, value)
         if not isinstance(value, bool):
             raise KiPlotConfigurationError('must be boolean')
         self._enabled = value
+        logger.warning(W_DEPR+'The `erc_warnings` preflight is deprecated, use the `warnings_as_errors` option')
 
     def get_example():
         """ Returns a YAML value for the example config """
