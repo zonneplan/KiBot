21a063d4414f198e7012119ff26e0d175cb65f48
Salvador E. Tropea <salvador@inti.gob.ar>
2024-05-03 07:35:01 -0300

    [Report][Added] Solder paste stats
    Solder paste margins ignored

--- kibot/misc.py
+++ kibot/misc.py
@@ -310,6 +310,7 @@ W_INV3DLAYER = '(W150) '
 W_NEEDSK8 = '(W151) '
 W_NEEDSK7 = '(W152) '
 W_NEEDSK6 = '(W153) '
+W_UNKPADSH = '(W154) '
 # Somehow arbitrary, the colors are real, but can be different
 PCB_MAT_COLORS = {'fr1': "937042", 'fr2': "949d70", 'fr3': "adacb4", 'fr4': "332B16", 'fr5': "6cc290"}
 PCB_FINISH_COLORS = {'hal': "8b898c", 'hasl': "8b898c", 'imag': "8b898c", 'enig': "cfb96e", 'enepig': "cfb96e",
