471770924f49f9a1ee8f714d07725ce2c2753c63
Salvador E. Tropea <salvador@inti.gob.ar>
2024-06-18 08:29:44 -0300

    [QR Lib][Changed] Missing QR definition no longer fatal

--- kibot/misc.py
+++ kibot/misc.py
@@ -316,6 +316,7 @@ W_NODESC = '(W156) '
 W_NOPAGES = '(W157) '
 W_NOLAYERS = '(W158) '
 W_NOPOPMD = '(W159) '
+W_NOQR = '(W160) '
 # Somehow arbitrary, the colors are real, but can be different
 PCB_MAT_COLORS = {'fr1': "937042", 'fr2': "949d70", 'fr3': "adacb4", 'fr4': "332B16", 'fr5': "6cc290"}
 PCB_FINISH_COLORS = {'hal': "8b898c", 'hasl': "8b898c", 'imag': "8b898c", 'enig': "cfb96e", 'enepig': "cfb96e",
