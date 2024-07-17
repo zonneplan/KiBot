f94c46f048aca4ddcae9ee3f0d2135aa8037c398
Salvador E. Tropea <salvador@inti.gob.ar>
2024-07-17 13:26:15 -0300

    [Tests] Image Magic site still down

--- tests/test_plot/test_dep_downloader.py
+++ tests/test_plot/test_dep_downloader.py
@@ -140,8 +140,9 @@ def test_dep_gs(test_dir, caplog, monkeypatch):
     os.remove(os.path.join(ctx.output_dir, bin_dir, 'gs'))
 
 
-@pytest.mark.xfail(True, reason="URL down", run=True, raises=AssertionError)
+# @pytest.mark.xfail(True, reason="URL down", run=True, raises=AssertionError)
 # https://imagemagick.org/archive/binaries/magick
+@pytest.mark.skip
 @pytest.mark.slow
 @pytest.mark.indep
 def test_dep_convert(test_dir, caplog, monkeypatch):
