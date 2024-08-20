2f160b1d3650dcd632347f169e03f700e3ab3d35
Salvador E. Tropea <salvador@inti.gob.ar>
2024-08-20 09:35:56 -0300

    [Changed] extra argument for generate_outputs
    We don't use it

--- kibot/__main__.py
+++ kibot/__main__.py
@@ -571,7 +571,7 @@ def main():
         generate_makefile(args.makefile, plot_config, outputs)
     else:
         # Do all the job (preflight + outputs)
-        generate_outputs(outputs, args.target, args.invert_sel, args.skip_pre, args.cli_order, args.no_priority,
+        generate_outputs(args.target, args.invert_sel, args.skip_pre, args.cli_order, args.no_priority,
                          dont_stop=args.dont_stop)
     # Print total warnings
     logger.log_totals()
