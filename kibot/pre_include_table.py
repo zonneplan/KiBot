eee0c902e05dc02f0c2640e8659a41ca5a89e544
Salvador E. Tropea <salvador@inti.gob.ar>
2024-10-24 08:14:58 -0300

    [Added] Copyright message to @nguyen-v preflights
    Gives more clarity to the "copywrong" use of AGPL

--- kibot/pre_include_table.py
+++ kibot/pre_include_table.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2024 Nguyen Vincent
+# Copyright (c) 2024 Salvador E. Tropea
+# Copyright (c) 2024 Instituto Nacional de Tecnología Industrial
 # License: AGPL-3.0
 # Project: KiBot (formerly KiPlot)
 # Contributed by Nguyen Vincent (@nguyen-v)
